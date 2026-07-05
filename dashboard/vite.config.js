8629c46837cf8d281330ee72165d29a1013312e5
Ntuli Nonhlnhla Mbali <mbalinonhlanhla684@gmail.com>
2026-07-05 11:01:13 +0200

    Update vite.config.js

--- dashboard/vite.config.js
+++ dashboard/vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    allowedHosts: ['dashboard-production-3c67.up.railway.app'],
+  },
 })
